edde19d95bdecc3ef91b58faa3b05737be3c063b
camillyPessotti <camilly_pessotti@estudante.sc.senai.br>
2022-06-20 20:59:35 -0300

    commit

--- crud/index.js
+++ crud/index.js
@@ -1,29 +1,45 @@
 const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-initializeApp({
-    credential: applicationDefault()
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyCMfYydh79dinleDXYyRRKm5S7cLjIATEg",
+    authDomain: "servidor-com-banco-de-dados.firebaseapp.com",
+    projectId: "servidor-com-banco-de-dados",
+    storageBucket: "servidor-com-banco-de-dados.appspot.com",
+    messagingSenderId: "632108984809",
+    appId: "1:632108984809:web:cf20fb6c246e3f41c34378",
+    measurementId: "G-PVR5DW0PJX"
+  };
+
+initializeApp(firebaseConfig);
 
 const db = getFirestore();
 
-const docRef = db.collection('users').doc('alovelace');
-
-await docRef.set({
-    first: 'Ada',
-    last: 'Lovelace',
-    born: 1815
-});
-
 async function save(nomeTabela, id, dado) {
     if (id) {
+        const referenceEntity = await db.collection(nomeTabela).doc(id).set(dado);
+        const savedDado = {
+            ... dado,
+            id: id
+        }
 
+        return savedData;
     } else {
         const referenceEntity = await db.collection(nomeTabela).add(dado);
         const savedData = {
-            ... dado
-        }
+            ... dado,
+            id: referenceEntity.id
+        };
+
+        return savedData;
     };
 };
 
 //coleção = tabela
+
+module.exports = {
+    save
+}
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ sandBox.js
@@ -0,0 +1,3 @@
+const crud = require("./crud");
+
+crud.save("pessoas", undefined, { nome: "Camiss", sobrenome: "Pessotti" });
